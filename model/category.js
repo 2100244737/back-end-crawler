a09492b806d23df10f18c5a49424569a7a91236f
song <2100244737@qq.com>
2019-06-19 11:36:48 +0800

    '06-19上午，添加分类书籍'

---
+++ model/category.js
@@ -0,0 +1,19 @@
+// 分类模块集合
+const mongoose = require('mongoose');
+const category = mongoose.Schema({
+     title: String,
+     icon: String,
+     books: [{
+         type: mongoose.SchemaTypes.ObjectId,
+         ref: 'book'
+     }],
+    index: {
+         type: Number,
+         default: 1
+    },
+    status: {
+         type: Number,
+         default: 1
+    }
+},{versionKey: false, timestamp: {createdAt:'createTime', updatedAt: 'updateTime'}})
+module.exports = mongoose.model('category', category)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const bookRouter = require('./book')
-router.use('/book', bookRouter)
+const categoryRouter = require('./category')
+router.use('/book', bookRouter) // 爬取一本书
+router.use('/category', categoryRouter) // 添加和获取分类
 module.exports = router;
